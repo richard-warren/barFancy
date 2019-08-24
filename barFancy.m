4ed106c27f915c7606ed84839f2d40e6882c7acb
rwarren2163 <richardwarren2163@gmail.com>
2019-08-24 12:45:32 -0400

    refactored, removed stats options, added documentation

--- barFancy.m
+++ barFancy.m
@@ -1,107 +1,132 @@
 function barFancy(data, varargin)
 
-% todo: replace my hacky code with combve
+% OVERVIEW:
+% Make fancy bar plots for multi-factor, multi-level data. Find full
+% documentation at https://github.com/rwarren2163/fancyPlots
+%
+% Generates bat plots for data with >=1 FACTORS (gender, hair color),
+% each with >=2 LEVELS (e.g. {male, female}, {black, brown, 
+% blonde}. Each bar is a CONDITION (male, black hair), and the hierarchical
+% relationship between condition iis automatically preserved.
+%
+% EXAMPLES:
+% barFancy(data, 'showViolins', true, 'levelNames', {{'male', 'female'}, {'black', 'brown', 'blonde'}})
+% Run barFancy_demo to see examples of usage
+%
+% INPUTS:
+% data (required): Matrix where each dimension is a FACTOR, and the final
+% dimension contains data for each sample. The size of each dimension is
+% the number of levels for each factor. For example, to plot data grouped
+% by gender (male, female) and hair color (black, brown, blonde), data
+% would be a 2 X 3 X number of sample matrix
+%
+% optional inputs: Name value pairs can be passed to overwrite thhe default
+% values in the structure 's'. See the comments in 'settings' below for a
+% full list of the many options available.
+
+% todo: output graph handle?
+
 
 % SETTINGS
 
 % bar settings
-s.showBars = false;  % add vertical bars instead of just horizontal line at mean
+s.showBars = true;            % add vertical bars instead of just horizontal line at mean for each condition
 s.barAlpha = .2;
 s.showErrorBars = true;
-s.colors = [.2 .2 .2]; % !!!
-s.showViolins = false;
-s.violinAlpha = .2; % alpha for violin plot fill
+s.errorFunction = @std;       % can change to custom error function, e.g. standard error instead of standard deviation
+s.colors = [.2 .2 .2];        % bar colors // can be name of matlab color space (e.g. 'hsv') OR nX3 matrix of colors where each row is the color of a specific condition
+s.showViolins = false;        % add vertical probability density estimates, creating a 'violin plot'
+s.violinAlpha = .2;
 s.barSeparation = 1;
 s.barWidth = 1;
 s.lineThickness = 3;
 
 % scatter settings
-s.showScatter = true;
-s.scatColors = 'hsv'; % can be a single rgb value, a name of a color space, or a matrix of colors
-s.circSize = 40;
-s.scatAlpha = .6;
+s.isRepeatedMeasures = true;  % whether same samples are represted across conditions (i.e. the design is 'within subjects')
+s.showScatter = true;         % scatter the values of individual samples
+s.scatterColors = 'hsv';      % scatter colors // can be name of matlab color space (e.g. 'hsv') OR nX3 matrix of colors where each row is the color of a particular sample
+s.scatterSize = 40;
+s.scatterAlpha = .6;
 
 % labels
-s.conditionNames = {}; % nested cell array, where each cell array contains names of the levels of each variable
+s.levelNames = {};            % names of levels for each factor // cell array of cell arrays where each nested array contains names of the levels for a particular factor, e.g. {{'male', 'female'}, {'tall', 'short'}}
 s.ylabel = [];
 
-% stats settings
-s.showStats = true;
-s.compareToFirstOnly = true; % only run stats between first and all other conditions
-s.isWithinSubs = true; % are the conditions within subjects
-s.pThresh = .05;
 
 
+
+% INITIALIZATIONS
 
 % reassign settings passed in varargin
 if exist('varargin', 'var'); for i = 1:2:length(varargin); s.(varargin{i}) = varargin{i+1}; end; end
 
-% initializations
-numVariables = length(size(data))-1;
-varLevelNum = size(data); varLevelNum = varLevelNum(1:end-1); % number of levels for each variable
-totalConditions = prod(varLevelNum);
+% determine number of factors, levels, and conditions
+numFactors = length(size(data))-1;
+numLevels = size(data); numLevels = numLevels(1:end-1); % number of levels for each variable
+numConditions = prod(numLevels);
 dataDims = size(data);
 
-if ischar(s.colors) % set bar colors if color is specified as a string
-    s.colors = eval([s.colors '(totalConditions)']);
+% set bar colors if color is specified as a string
+if ischar(s.colors)
+    s.colors = eval([s.colors '(numConditions)']);
 elseif isequal(size(s.colors), [1 3]) % if specified as a single rbg value, replicate into a matrix
-    s.colors = repmat(s.colors,totalConditions,1);
+    s.colors = repmat(s.colors,numConditions,1);
 end
 
-if ischar(s.scatColors) % set bar colors if color is specified as a string
-    if s.isWithinSubs
-        s.scatColors = eval([s.scatColors '(dataDims(end))']);
+% set scatter colors if color is specified as a string
+if ischar(s.scatterColors)
+    if s.isRepeatedMeasures
+        s.scatterColors = eval([s.scatterColors '(dataDims(end))']);
     else
-        s.scatColors = [.5 .5 .5];
+        s.scatterColors = [.5 .5 .5];
     end
 end
 
-conditionsMat = nan(numVariables, totalConditions);
-labelVertSize = .15*numVariables; % size of space below figure to give to to axis labels, expressed as fraction of y range
-statsVertSpacing = .02; % vertical spacing of stat comparison lines, expressed as fraction of y range
-xJitters = linspace(-.5*s.barWidth, .5*s.barWidth, dataDims(end));
+% determine various spatial parameters
+labelVertSize = .15*numFactors;  % size of space below figure to give to to axis labels, expressed as fraction of y range
+xJitters = linspace(-.5*s.barWidth, .5*s.barWidth, dataDims(end));  % jitters for scatter points
 xJitters = xJitters(randperm(length(xJitters)));
-hold on
 
-
-% create matrix where each column is an interection of conditions
-xPositions = 1:totalConditions;
-for i = 1:numVariables
-    repeats = prod(varLevelNum(i+1:end));
-    copies = totalConditions / (repeats*varLevelNum(i));
-    conditionsMat(i,:) = repmat(repelem(1:varLevelNum(i), repeats), 1, copies);
-    xPositions = xPositions + (repelem(1:copies*varLevelNum(i), repeats)-1) * s.barSeparation;
+% create matrix where each row is a factor, each entry is a level for a
+% given factor, and each column is a condition (i.e.a unique combination
+% of factor levels)
+conditionsMat = nan(numFactors, numConditions);
+xPositions = 1:numConditions;
+for i = 1:numFactors
+    repeats = prod(numLevels(i+1:end));
+    copies = numConditions / (repeats*numLevels(i));
+    conditionsMat(i,:) = repmat(repelem(1:numLevels(i), repeats), 1, copies);
+    xPositions = xPositions + (repelem(1:copies*numLevels(i), repeats)-1) * s.barSeparation;
 end
 
+
+
+
+% GENERATE PLOT
+hold on
+
 % add lines connecting same sample across conditions
-if s.isWithinSubs && dataDims(end)<40 
-    [~,~,condInds] = unique(conditionsMat(1:end-1,:)', 'rows'); % only draw lines connecting data across last condition
-    
-    for i = 1:dataDims(end) % for each subject        
+if s.isRepeatedMeasures && dataDims(end)<100  % latter term prevents drawing lines with there are a large number of samples
+    for i = 1:dataDims(end)  % loop across samples
         
-        % get data in all conditions
-        smpData = nan(1,totalConditions);
-        for j = 1:totalConditions
+        % get data for sample in all conditions
+        smpData = nan(1,numConditions);
+        for j = 1:numConditions
             inds = num2cell([conditionsMat(:,j); i]);
             smpData(j) = squeeze(data(inds{:}));
         end
-        
-        % draw lines connecting data only across levels of last condition
-        for j = unique(condInds)'
-            line(xPositions(condInds==j)+xJitters(i), smpData(condInds==j), ...
-                'linewidth', 1, 'color', [.8 .8 .8 s.scatAlpha]);
-        end
+
+        line(xPositions+xJitters(i), smpData, 'linewidth', 1, 'color', [.8 .8 .8 s.scatterAlpha]);
     end
 end
 
-
-% plot data
-allData = cell(1,totalConditions);
-for i = 1:totalConditions
-    inds = cat(1, num2cell(conditionsMat(:,i)), {1:size(data,length(dataDims))});
+% make bars, etc
+allData = cell(1,numConditions);  % each entry contains a vector of values for all samples within a condition
+for i = 1:numConditions
+    
+    inds = cat(1, num2cell(conditionsMat(:,i)), {1:size(data,ndims(data))});  % inds for this condition within data matrix
     condData = squeeze(data(inds{:}));
     allData{i} = condData;
-    if s.isWithinSubs; lineColor = [0 0 0]; else; lineColor = scatColors(conditionsMat(end,i),:); end
     
     % add probability density estimate
     if s.showViolins
@@ -114,11 +139,12 @@ for i = 1:totalConditions
     % scatter raw data
     if s.showScatter
         scatter(xJitters + xPositions(i), condData, ...
-            s.circSize, s.scatColors, 'filled', 'MarkerFaceAlpha', s.scatAlpha); hold on
+            s.scatterSize, s.scatterColors, 'filled', 'MarkerFaceAlpha', s.scatterAlpha); hold on
     end
     
+    % add error bars
     if s.showErrorBars
-        err = nanstd(condData);
+        err = s.errorFunction(condData);
         line([xPositions(i) xPositions(i)], [err -err] + nanmean(condData), ...
             'color', s.colors(i,:), 'linewidth', s.lineThickness*.5)
     end
@@ -130,55 +156,11 @@ for i = 1:totalConditions
     end
 end
 
-
-% get y limit information
-yLims = get(gca, 'ylim');
-
-% add pairwise stats
-if s.showStats
-    [~,~,condInds] = unique(conditionsMat(1:end-1,:)', 'rows'); % only draw lines connecting data across last condition
-    
-    for i = unique(condInds)'
-        dimPairs = nchoosek(1:length(find(condInds==i)), 2); % wrt data dimensions
-        condPairs = nchoosek(find(condInds==i), 2); % wrt columns in plot
-        
-        % sort s.t. more distant comparisons are last
-        [~, sortInds] = sort(diff(dimPairs,[],2));
-        dimPairs = dimPairs(sortInds,:); condPairs = condPairs(sortInds,:);
-        
-        % only keep comparisons between first and all other conditions
-        if s.compareToFirstOnly
-            bins = any(dimPairs==1,2);
-            dimPairs = dimPairs(bins,:);
-            condPairs = condPairs(bins,:);
-        end
-        
-        % vertical position of each line, expressed as fraction of y range
-        yMax = yLims(2) + statsVertSpacing*range(yLims)*size(dimPairs,1);
-        yPosits = linspace(yLims(2), yMax, size(dimPairs,1)); 
-        inds = conditionsMat(1:end-1, find(condInds==i,1,'first'))'; % matrix inds for conditions higher up in the hierarchy
-        
-        for j = 1:size(dimPairs,1)
-            
-            [inds1, inds2] = deal(cat(2,num2cell([inds dimPairs(j,1)]), {1:size(data,length(dataDims))}));
-            inds2{numVariables} = dimPairs(j,2);
-            
-            if s.isWithinSubs
-                [~,p] = ttest(data(inds1{:}), data(inds2{:}));
-            else
-                [~,p] = ttest2(data(inds1{:}), data(inds2{:}));
-            end
-            if p<s.pThresh; lineColor = 'red'; else; lineColor = [.5 .5 .5]; end
-            line(xPositions(condPairs(j,:)), [yPosits(j) yPosits(j)], ...
-                'color', lineColor, 'linewidth', 1.0);
-        end
-    end
-end
-
+yLims = get(gca, 'ylim');  % get initial y limits (which will be subsequently adjusted)
 
 % add bars
 if s.showBars
-    for i = 1:totalConditions
+    for i = 1:numConditions
         x = [-.5 .5]*s.barWidth + xPositions(i);
         y = [yLims(1), nanmean(allData{i})];
         plot([x(1) x(1) x(2) x(2)], [y(1) y(2) y(2) y(1)], ...
@@ -195,28 +177,39 @@ end
 
 
 
+
+% ADD X AXIS LABELS
+
+% add room below figure for labels
+figColor = get(gcf, 'color');
+if ~isempty(s.levelNames)
+    yMin = yLims(1)-labelVertSize*range(yLims);
+    line([0 0], [yMin, yLims(1)], 'color', figColor, 'linewidth', 3) % cover bottom of y axis with white line
+    yLims = [yMin, yLims(2)];
+end
+
 % add labels
-for i = 1:length(s.conditionNames)
+for i = 1:length(s.levelNames)
     
     parentConditions = unique(conditionsMat(1:i-1,:)','rows');
     
     for j = 1:size(parentConditions,1)
         
-        if i==1; bins=true(1,totalConditions)'; else; bins = ismember(conditionsMat(1:i-1,:)', parentConditions(j,:), 'rows'); end
+        if i==1; bins=true(1,numConditions)'; else; bins = ismember(conditionsMat(1:i-1,:)', parentConditions(j,:), 'rows'); end
         
-        for k = 1:varLevelNum(i)
+        for k = 1:numLevels(i)
             inds = find(conditionsMat(i,:)==k & bins');
             xPos = mean(xPositions(inds));
             yPos = yLims(1)-labelVertSize*range(yLims) + ((labelVertSize*range(yLims))/length(dataDims)*i);
-            if i==numVariables; rotation = 25; else; rotation = 0; end
-            if ~isempty(s.conditionNames)
-                condText = text(xPos, yPos, s.conditionNames{i}(k), 'rotation', rotation, ...
+            if i==numFactors; rotation = 25; else; rotation = 0; end
+            if ~isempty(s.levelNames)
+                condText = text(xPos, yPos, s.levelNames{i}(k), 'rotation', rotation, ...
                     'HorizontalAlignment', 'center', 'VerticalAlignment', 'middle');
             end
             
             % add lines on the side of condition name
-            if i<numVariables
-                if ~isempty(s.conditionNames)
+            if i<numFactors
+                if ~isempty(s.levelNames)
                     textPos = get(condText, 'Extent');
                     line([xPositions(inds(1)) textPos(1)], [yPos yPos], 'color', [.5 .5 .5]) % left side of text
                     line([textPos(1)+textPos(3) xPositions(inds(end))], [yPos yPos], 'color', [.5 .5 .5]) % right side of text
@@ -226,22 +219,6 @@ for i = 1:length(s.conditionNames)
             end
         end
     end
-end
-
-
-% add room above figure for stat lines
-figColor = get(gcf, 'color');
-if s.showStats
-    line([0 0], [yLims(2), yMax], 'color', figColor, 'linewidth', 3) % cover top of y axis with white line
-    yLims = [yLims(1), yMax];
-end
-
-
-% add room below figure for labels
-if ~isempty(s.conditionNames)
-    yMin = yLims(1)-labelVertSize*range(yLims);
-    line([0 0], [yMin, yLims(1)], 'color', figColor, 'linewidth', 3) % cover bottom of y axis with white line
-    yLims = [yMin, yLims(2)];
 end
 
 set(gca, 'YLim', yLims, 'XLim', [0 xPositions(end)+1], ...
@@ -255,7 +232,7 @@ if ~isempty(s.ylabel)
     set(lab, 'position', labPos);
 end
 
-pause(.001) % when doing many subplots, this makes sure they pop up one by one
+pause(.001)
 
 
 
